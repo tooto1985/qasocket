e6f193d1556bc32fe60139dd45b43c8462f5e0fd
tommy <tooto1985@gmail.com>
2016-01-25 15:11:10 +0800

    v 1.0

--- app.js
+++ app.js
@@ -2,11 +2,86 @@ var express = require("express");
 var app = express();
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
+var topic = "";
 var users = [];
-app.use(express.static(__dirname+"/public"));
-io.on("connection", function (socket) {
-  socket.on('login', function (data) {
-    users.push(data);
-  });
+var SOCKET = null;
+var timeout = 5000;
+app.use(express.static(__dirname + "/public"));
+io.on("connection", function(socket) {
+	SOCKET = socket;
+	socket.on("admin_settopic", function(data) {
+		topic = data;
+		socket.broadcast.emit("client_newtopic", topic);
+		for (var i = 0; i < users.length; i++) {
+			users[i].result = null;
+			users[i].question = "";
+		}
+	});
+	socket.on("client_login", function(data) {
+		if (users.some(function(a) {
+				return a.name === data;
+			})) {
+			socket.emit("client_repeatname", data);
+		} else {
+			users.push({
+				name: data,
+				time: new Date().getTime(),
+				result: null,
+				question: ""
+			});
+			if (topic) {
+				socket.emit("client_newtopic", topic);
+			}
+			count();
+		}
+	});
+	socket.on("client_keep", function(data) {
+		for (var i = 0; i < users.length; i++) {
+			if (users[i].name === data) {
+				users[i].time = new Date().getTime();
+			}
+		}
+		count();
+	});
+	socket.on("client_result", function(data) {
+		for (var i = 0; i < users.length; i++) {
+			if (users[i].name === data.name) {
+				users[i].result = data.result;
+			}
+		}
+
+	});
+	socket.on("client_question", function(data) {
+		for (var i = 0; i < users.length; i++) {
+			if (users[i].name === data.name) {
+				users[i].question = data.question;
+			}
+		}
+		question();
+	});
 });
+
+function count() {
+	SOCKET.broadcast.emit("count", {
+		total: users.length,
+		completed: users.filter(function(a) {
+			return a.result === true;
+		}).length
+	});
+}
+
+function question() {
+	SOCKET.broadcast.emit("admin_getquestion", users.filter(function(a) {
+		return a.question !== "" && new Date().getTime() - a.time < timeout;;
+	}));
+}
+setInterval(function() {
+	users = users.filter(function(a) {
+		return new Date().getTime() - a.time < timeout;
+	});
+	if (SOCKET) {
+		count();
+		question();
+	}
+}, 3000);
 server.listen(3000);
--- public/index.js
+++ public/index.js
@@ -1,18 +1,88 @@
 $(function() {
 	var socket = io.connect();
-	var loginname;
-	$(".loginform").submit(function(e) {
-		loginname = $(".loginname").val();
-		if (loginname !== "") {
-			$(".login").hide();
-			$(".control").show();
-			$(".controlname").text(loginname);
-			setInterval(function() {
-				socket.emit('login', loginname);
-			},10000);
+
+	function admin() {
+		$(".nexttopic").click(function() {
+			socket.emit("admin_settopic", $(".admin .topic").val());
+			$(".list").html("");
+		});
+		socket.on("admin_getquestion", function(data) {
+			var html = "";
+			for (var i = 0; i < data.length; i++) {
+				html += "<div>" + data[i].name + ":" + data[i].question + "</div>";
+			}
+			$(".list").html(html);
+		});
+		socket.on("count", function(data) {
+			$(".totalCount").text(data.total);
+			$(".completedCount").text(data.completed);
+		});
+	}
+
+	function show(name) {
+		$(".login,.wait,.result").hide();
+		$("." + name).show();
+	}
+
+	function client() {
+		var name;
+		$(".form").submit(function(e) {
+			name = $(".login .name").val();
+			if (name !== "") {
+				show("wait");
+				setInterval(function() {
+					socket.emit("client_keep", name);
+				}, 3000);
+				socket.emit("client_login", name);
+			} else {
+				alert("請輸入名稱");
+			}
+			e.preventDefault();
+		});
+		$("#completed").change(function() {
+			socket.emit("client_result", {
+				name: name,
+				result: $(this).is(":checked")
+			});
+		});
+		$(".question").keyup(function() {
+			socket.emit("client_question", {
+				name: name,
+				question: $(this).val()
+			});
+		});
+		socket.on("client_newtopic", function(data) {
+			if (name) {
+				if (data) {
+					show("result");
+					$(".result .name").text(name);
+					$(".result .topic").text(data);
+					$("#completed").attr("checked", false);
+					$(".question").val("");
+				} else {
+					show("wait");
+				}
+			}
+		});
+		socket.on("client_repeatname", function(data) {
+			alert("重複名稱");
+			show("login");
+			name=null;
+		});
+		socket.on("disconnect", function() {
+			alert("伺服器斷線");
+			show("login");
+			name=null;
+		});
+	}
+	(function() {
+		if (location.search === "?admin") {
+			$(".admin").show();
+			admin();
 		} else {
-			alert("請輸入名稱");
+			$(".client").show();
+			client();
 		}
-		e.preventDefault();
-	});
+	})(); //init
+
 });
